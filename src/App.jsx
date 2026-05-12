4159fb19729254c396b8ea15e889bddac0ff74d8
SilvaAdenilton <adenilton.silva@hexagon.com>
2026-05-12 17:26:02 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Dashboard Jira</h1>
+        <p>Projeto funcionando corretamente no GitHub + Vercel.</p>
+      </div>
+    </div>
+  );
+}
